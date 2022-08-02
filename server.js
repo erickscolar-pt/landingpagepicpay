2c44ca78adc5dfd7209dcb10f7bad25e290e1a30
Erick Scolar <erick.scolar@outlook.com>
2022-08-01 23:07:25 -0300

    Name PicPay

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const caminho = require('caminho');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-name>'));
+app.use(express.static(__dirname + '/dist/picpay'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/picpay/index.html'));});
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'landingpagepicpay'`, () => {
+  it(`should have as title 'picpay'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('landingpagepicpay');
+    expect(app.title).toEqual('picpay');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('landingpagepicpay app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('picpay app is running!');
   });
 });
